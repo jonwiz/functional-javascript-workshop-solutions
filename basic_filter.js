3c921561b56731bebd93f2b1462d54094b1ab3cc
jonwiz <jonwiz@users.noreply.github.com>
2015-03-01 00:14:38 -0500

    solved basic filter

---
+++ basic_filter.js
@@ -0,0 +1,9 @@
+module.exports = function shortMessages(messages) {
+	
+	return messages.filter(function(obj) {
+		return obj.message.length < 50;
+	}).map(function(obj) {
+		return obj.message
+	});
+
+};
